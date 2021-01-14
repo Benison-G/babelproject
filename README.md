1938065d77157d732eb41712f5e0687084b8ef40
Gnanamithra, Benison <Benison.Gnanamithra@cerner.com>
2021-01-14 09:14:12 +0530

    Upgrade to babel 7

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # babelproject
 
 # To ouput the result into a file
-npm run babel // compiles the js to es5
-npm run build // runs the server
+`npm run babel` // compiles the js to es5 <br/>
+`npm run build` // runs the server <br/>
 
-On Babel 7 release, all the packages needs to be installed with @babel/ prefix
-ex: @babel/core
+On Babel 7 release, all the packages needs to be installed with `@babel/` prefix ex: `@babel/core` 
+
+## 3 Main dependencies:
+`@babel/cli` <br/>
+`@babel/core` <br/>
+`@babel/preset-env` <br/>
+
+## Output to a file babel
+`babel sourceFile.js --out-file targetFile.js`
+
+### .babelrc:
+`"presets": [ "@babel/env" ]`
--- dev/scripts.bundle.js
+++ dev/scripts.bundle.js
@@ -1,25 +1,43 @@
 "use strict";
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Student = function () {
-   function Student(fname, lname, age, address) {
-      _classCallCheck(this, Student);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-      this.fname = fname;
-      this.lname = lname;
-      this.age = age;
-      this.address = address;
-   }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-   _createClass(Student, [{
-      key: "fullname",
-      get: function get() {
-         return this.fname + "-" + this.lname;
-      }
-   }]);
+var Student = /*#__PURE__*/function () {
+  function Student(fname, lname, age, address) {
+    _classCallCheck(this, Student);
 
-   return Student;
-}();
+    this.fname = fname;
+    this.lname = lname;
+    this.age = age;
+    this.address = address;
+  }
+
+  _createClass(Student, [{
+    key: "fullname",
+    get: function get() {
+      return this.fname + "-" + this.lname;
+    }
+  }]);
+
+  return Student;
+}(); // const compile using babel
+
+
+var a = 1;
+
+if (a == 1) {
+  var _age = 10;
+}
+
+console.log(age); // let compile using babel
+
+if (a == 1) {
+  var _a = 2;
+  console.log(_a);
+}
+
+console.log(a);
